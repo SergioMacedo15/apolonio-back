79c8c69bd8fbf7d74c4c421ddef5b7e8b15795b8
Sergio Silva Macedo <63246587+SergioMacedo15@users.noreply.github.com>
2025-05-17 17:58:57 -0300

    Feature: Integrando Router a página

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import { getBrowser } from "@/lib/puppeteer";
+import OpenAIService from "@/services/sdk";
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
+import type { Page } from "puppeteer";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+let page: Page | undefined;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const browser = await getBrowser();
+  page = page ? page : await browser.newPage();
+  const service = new OpenAIService("gpt-4.1-mini", "");
+
+  const result = service.buildStreamText({
+    messages: messages,
+    page,
+  });
+  console.log("RESPOSTA OPEN AI", result);
+  return result.toDataStreamResponse();
+}
