5ea4a5d9148d4f339fed16930165d219cdb3213e
Sergio Silva Macedo <63246587+SergioMacedo15@users.noreply.github.com>
2025-05-17 22:54:45 -0300

    Feature: Componentizando class de Service Api

--- src/services/sdk/index.ts
+++ src/services/sdk/index.ts
@@ -2,25 +2,7 @@ import { aiTools, type AIToolsProps } from "@/aiTools";
 import { openai } from "@ai-sdk/openai";
 import { streamText, tool, type StreamTextResult } from "ai";
 import { createStreamableValue } from "ai/rsc";
-
-type OpenAIModel =
-  | "gpt-4.1"
-  | "gpt-4.1-mini"
-  | "gpt-4.1-nano"
-  | "gpt-4.5"
-  | "gpt-4o"
-  | "gpt-4o-mini"
-  | "o3"
-  | "o3-mini"
-  | "o3-mini-high"
-  | "o4-mini"
-  | "o4-mini-high"
-  | "gpt-image-1"
-  | "whisper"
-  | "whisper-large-v2"
-  | "whisper-large-v3";
-
-type RoleAPIModel = "user" | "system";
+import type { OpenAIModel, RoleAPIModel } from "./types";
 
 interface ContextAPIProps {
   role: RoleAPIModel;
@@ -32,7 +14,6 @@ interface SendMessageProps {
 }
 interface BuildStreamTextProps {
   messages: ContextAPIProps[];
-  tools?: AIToolsProps; // MELHORAR ISSO AQUI
   page: any;
 }
 
@@ -41,49 +22,46 @@ export default class OpenAIService {
   private mood: string;
   public god: any;
   public stream: SendMessageProps | any;
+  private tools: object;
 
-  constructor(apiModel: OpenAIModel, mood: string) {
+  constructor(apiModel: OpenAIModel, mood: string, tools: object) {
     this.tModel = apiModel;
     this.mood = mood;
+    this.tools = tools;
   }
 
   buildStreamText({
     messages,
-    tools,
     page,
   }: BuildStreamTextProps): StreamTextResult<{}, never> {
     const streamTextComponent = streamText({
       model: openai(this.tModel),
       system: this.mood,
       messages,
-      tools: Object.entries(aiTools).reduce((acc, [name, info]) => {
-        return {
-          ...acc,
-          [name]: tool({
-            description: info.description,
-            parameters: info.schema,
-            execute: async (input) => {
-              const response = await info.execute(page, input);
-              console.log("executou", response);
-              return response || {};
-            },
-          }),
-        };
-      }, {}),
+      tools: this.buildToolsComponente(page),
     });
 
     return streamTextComponent;
   }
 
-  async streamMessage(textStream: any) {
-    try {
-      const stream = createStreamableValue();
-      for await (const text of textStream) {
-        stream.update(text);
-      }
-      this.stream = stream.done;
-    } catch (error) {
-      console.log("erro de envio de mensagem ", error);
-    }
+  buildToolsComponente(page: any) {
+    return Object.entries(this.tools).reduce((acc, [name, info]) => {
+      return {
+        ...acc,
+        [name]: tool({
+          description: info.description,
+          parameters: info.schema,
+          execute: async (input) => {
+            const response = await info.execute(page, input);
+            console.log("executou", response);
+            return response || {};
+          },
+        }),
+      };
+    }, {});
+  }
+
+  setSystem(system: string) {
+    this.mood = system;
   }
 }
