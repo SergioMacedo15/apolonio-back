ea07136e37bf57e278517d003878bf35f1f0618e
Gustavo-Cruz-Pinheiro <gcp2703@gmail.com>
2025-05-18 02:53:01 -0300

    feat: Responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,45 @@ import Header from "@/app/components/HeaderSidebar";
 import ConversationList from "@/app/components/ConversationList";
 import HeaderChat from "@/app/components/HeaderChat";
 import Chat from "@/app/components/Chat";
-import { useChat } from "@ai-sdk/react";
+import {useChat} from "@ai-sdk/react";
+import {useState, useEffect} from "react";
 
 export default function Home() {
-  return (
-    <div className="w-full h-full flex">
-      <aside className="w-[5%] border-r border-gray-600 bg-gray-900 text-white">
-        <Sidebar />
-      </aside>
-      <aside className="w-[28%] border-r border-gray-600 bg-gray-900 text-white">
-        <Header />
-        <ConversationList />
-      </aside>
+    const [showAsides, setShowAsides] = useState(true);
 
-      <div className="flex-1 flex flex-col">
-        <div className="bg-gray-800 h-[9.5%] text-white border-b border-gray-600">
-          <HeaderChat />
-        </div>
+    useEffect(() => {
+        function handleResize() {
+            setShowAsides(window.innerWidth >= 1300);
+        }
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-        <div className="flex-1">
-          <Chat />
+    return (
+        <div className="w-full h-full flex">
+            {showAsides && (
+                <aside className="w-[5%] border-r border-gray-600 bg-gray-900 text-white">
+                    <Sidebar />
+                </aside>
+            )}
+
+            {showAsides && (
+                <aside className="w-[28%] border-r border-gray-600 bg-gray-900 text-white">
+                    <Header />
+                    <ConversationList />
+                </aside>
+            )}
+
+            <div className="flex-1 flex flex-col">
+                <div className="bg-gray-800 h-[9.5%] text-white border-b border-gray-600">
+                    <HeaderChat />
+                </div>
+
+                <div className="flex-1">
+                    <Chat />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
