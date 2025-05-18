37595813205b6d93b261a73f4982d785a368ea8d
Sergio Silva Macedo <63246587+SergioMacedo15@users.noreply.github.com>
2025-05-18 01:08:59 -0300

    Feature: Integrando camada de chat com interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Sidebar from "@/app/components/Sidebar";
 import Header from "@/app/components/HeaderSidebar";
 import ConversationList from "@/app/components/ConversationList";
 import HeaderChat from "@/app/components/HeaderChat";
 import Chat from "@/app/components/Chat";
+import { useChat } from "@ai-sdk/react";
 
 export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const handleSendMessage = () => {};
   return (
     <div className="w-full h-full flex">
       <aside className="w-[5%] border-r border-gray-600 bg-gray-900 text-white">
@@ -21,7 +26,7 @@ export default function Home() {
         </div>
 
         <div className="flex-1">
-          <Chat />
+          <Chat onSend={handleSendMessage} messages={messages} />
         </div>
       </div>
     </div>
